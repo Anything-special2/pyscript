e94a65403e5f4aa7183b32a2fe539942d8b9a099
Anything-special2 <kristofflaliclalic@gmail.com>
2024-07-19 12:53:18 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
-
+function rotate() {
+    let navbar = document.querySelector(".navbar")
+    let section = document.querySelector(".section")
+    if (navbar.classList.contains("rotation")) {
+        navbar.classList.remove("rotation")
+        section.classList.add("sectionDown")
+    navbar.classList.add("reverse_rotation")
+    section.classList.remove("sectionUp")
+    } else {
+        navbar.classList.add("rotation")
+        section.classList.remove("sectionDown")
+        section.classList.add("sectionUp")
+   navbar.classList.remove("reverse_rotation")
+    }
+    
+}
